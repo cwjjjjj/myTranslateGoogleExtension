39eefab26b55c29aa6d40865fba7b764d69a81e1
cwjjjjj <93369602+cwjjjjj@users.noreply.github.com>
2022-12-07 16:29:31 +0800

    remove package json

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf build
-npx esbuild src/content-script/index.mjs src/background/index.mjs --bundle --outdir=build
-cp src/*.css build/
-cp src/*.json build/
-cp src/*.png build/
-cp src/*.html build/
-cp src/*.js build
